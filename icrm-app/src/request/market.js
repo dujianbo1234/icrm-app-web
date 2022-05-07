559f353e176cc1ab864afa82e9a6ae5b3752ab83
hujixian <156128904@qq.com>
2022-05-07 18:10:49 +0800

    我的商机页面接口调试

--- icrm-app/src/request/market.js
+++ icrm-app/src/request/market.js
@@ -4,3 +4,9 @@ import { request } from "./request.js";
 export const queryCommercialOpportunityCount = (params,fun) => {request("market.queryCommercialOpportunityCount", params, fun)}
 // 查询商机列表
 export const queryCommercialOpportunityList = (params,fun) => {request("market.queryCommercialOpportunityList", params, fun)}
+
+
+// 查询商机汇总
+export const queryCmrcOpportunitySum = (params,fun) => {request("market.queryCmrcOpportunitySum", params, fun)}
+// 查询商机汇总列表
+export const queryCmrcOpportunitySumList = (params,fun) => {request("market.queryCmrcOpportunitySumList", params, fun)}
