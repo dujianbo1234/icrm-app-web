ee69cd1bf39493338ee4ae5475754f37100c8b13
hujixian <156128904@qq.com>
2022-05-12 15:16:30 +0800

    main.js

--- icrm-app/src/main.js
+++ icrm-app/src/main.js
@@ -1,6 +1,5 @@
 import {createApp} from 'vue';
 import App from './App.vue';
-import changePwd from './changePwd.vue';
 import router from './router';
 import store from './store';
 import VConsole from 'vconsole'
