31f694051c77e31ea33fb5fc401eea4e39f76301
dujainbo1234 <461954137@qq.com>
2022-05-12 10:13:25 +0800

    添加webpack打包时环境变量的判断

--- icrm-app/vue.config.js
+++ icrm-app/vue.config.js
@@ -1,5 +1,5 @@
 const webpack = require('webpack');
-
+const isDev = process.env.NODE_ENV === 'development';
 module.exports = {
   publicPath: './',
   configureWebpack: {
@@ -7,7 +7,8 @@ module.exports = {
       new webpack.DefinePlugin({
         'process.env': {
           BASE_URL: JSON.stringify(process.env.BASE_URL),
-          CLIENT_NAME: JSON.stringify(process.env.CLIENT_NAME)
+          CLIENT_NAME: JSON.stringify(process.env.CLIENT_NAME),
+          NODE_ENV: isDev ? "development" : "production"
         }
       }),
     ]
