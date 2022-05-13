6e831cb0ca19bbd4b46502e551708b841f0fb38a
dujainbo1234 <461954137@qq.com>
2022-05-13 11:28:44 +0800

    table样式修改,第一列固定

--- icrm-app/src/views/index/components/Table.vue
+++ icrm-app/src/views/index/components/Table.vue
@@ -1,26 +1,45 @@
 <template>
   <div class="Table">
     <table border="0" cellspacing="0" cellpadding="0">
-      <!-- 表头 -->
-      <tr height="32">
-        <th v-for="item in listLabel" :key="item" :align="item.align">{{ item.label }}</th>
-      </tr>
-      <tr v-for="item in listData" :key="item.name" height="32">
-        <td align="left" valign="middle">{{item.name}}</td>
-        <td align="right" valign="middle" v-for="name in listArr" :key="name">
-          <span class="textS">
-            <span :style="{'color' : (item[name] > 0 && name != 'a') ? '#FF3A3A' : (item[name] < 0 ? ' #37CD37' : '#595959')}">{{numFliter(item[name], name != 'a')}}</span>
-            <template v-if="name != 'a'">
-              <!-- 上升 -->
-              <van-icon :name="require('@/assets/image/index_arrow_top.png')" size="0.16rem" v-if="item[name] > 0"/>
-              <!-- 下降icon -->
-              <van-icon :name="require('@/assets/image/index_arrow_dowm.png')" size="0.16rem" v-else-if="item[name] < 0"/>
-              <!-- 无数据 -->
-              <van-icon :name="require('@/assets/image/index_main_numLine.png')" size="0.16rem" v-else/>
+      <div class="left">
+        <!-- 表头 -->
+        <tr height="32" valign="middle">
+          <template v-for="(item,index) in listLabel">
+            <th :align="item.align" :key="item" v-if="index == 0">
+              <span class="text">{{ item.label }}</span>
+            </th>
+          </template>
+        </tr>
+        <!-- 第一列 -->
+        <tr v-for="item in listData" :key="item.name" height="32">
+          <td align="left" valign="middle">
+            <span class="text">{{ item.name }}</span>
+          </td>
+        </tr>
+      </div>
+      <div ref="right" class="right">
+        <div class="text">
+          <!-- 表头 -->
+          <tr height="32" valign="middle">
+            <template v-for="(item,index) in listLabel">
+              <th :align="item.align" :key="item" v-if="index > 0"><span class="content">{{ item.label }}</span></th>
             </template>
-          </span>
-        </td>
-      </tr>
+          </tr>
+          <!-- 除第一列 -->
+          <tr v-for="item in listData" :key="item.name" height="32">
+            <td align="right" valign="middle" v-for="name in listArr" :key="name">
+              <span class="textS">
+                <span class="content" :style="{'color' : (item[name] > 0 && name != 'a') ? '#FF3A3A' : (item[name] < 0 ? ' #37CD37' : '#595959')}">{{numFliter(item[name], name != 'a')}}</span>
+                <template v-if="name != 'a'">
+                  <van-icon :name="require('@/assets/image/index_arrow_top.png')" size="0.16rem" v-if="item[name] > 0"/>
+                  <van-icon :name="require('@/assets/image/index_arrow_dowm.png')" size="0.16rem" v-else-if="item[name] < 0"/>
+                  <van-icon :name="require('@/assets/image/index_main_numLine.png')" size="0.16rem" v-else/>
+                </template>
+              </span>
+            </td>
+          </tr>
+        </div>
+      </div>
     </table>
   </div>
 </template>
@@ -45,6 +64,7 @@ export default {
       }else{
         this.listArr = ['a','b','c', 'd']
       }
+      this.$refs['right'].scrollLeft = 0
     }
   },
   methods: {
@@ -69,9 +89,25 @@ export default {
 .Table {
   font-size: 0.12rem;
   width: 3.27rem;
-  overflow-x: scroll;
   &:deep(table) {
-    min-width: 4.27rem;
+    display: flex;
+    .left {
+      width: 24%;
+      box-shadow: 0.005rem 0.005rem 0.03rem 0 rgba(0,0,0,0.1);
+      th, th {
+        min-width: 0.8rem;
+      }
+    }
+    .right {
+      width: 71%;
+      overflow-x: scroll;
+      &::-webkit-scrollbar {
+        display: none;
+      }
+      .content {
+        min-width: 0.85rem;
+      }
+    }
     th, td{
       word-break: keep-all;
       word-wrap:break-word;
