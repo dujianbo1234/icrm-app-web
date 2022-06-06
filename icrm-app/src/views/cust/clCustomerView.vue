360611c7ced19281ffeabe9018c67c92d9577b41
dujainbo1234 <461954137@qq.com>
2022-06-06 11:32:40 +0800

    删除默认初始化

--- icrm-app/src/views/cust/clCustomerView.vue
+++ icrm-app/src/views/cust/clCustomerView.vue
@@ -1401,13 +1401,6 @@ export default {
   },
 };
 </script>
-<style scoped>
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
-</style>
 <style lang="less" scoped>
 .clCustomerView {
   box-sizing: border-box;
